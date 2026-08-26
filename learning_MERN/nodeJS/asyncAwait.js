d296043d7da04b809c86c588ffd1fbc50564d1b8
rishit_06 <rishitrajput2006@gmail.com>
2026-08-26 13:52:27 +0530

    promises and async

---
+++ learning_MERN/nodeJS/asyncAwait.js
@@ -0,0 +1,6 @@
+async function getUsers() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await response.json();
+    console.log(data);
+}
+getUsers();
